42ab0267d6fd11743ef710880e8b08402aa0a051
admsgit <89482610+admsgit@users.noreply.github.com>
2021-10-21 17:23:02 +0530

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -6,6 +6,14 @@ import re
 import pandas as pd
 import numpy as np
 
+from google_play_scraper import app
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk import FreqDist
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import yagmail
+
 numVideosToAnalyse=10
 users_url="https://abhigyanresources.com/vwusers"
 
@@ -222,31 +230,193 @@ def validateUser(user_email):
         else:
             st.title("Invalid email id. Check and try again.")
 
+def gplayDetails(app_url):
 
-#Render the page 
-st.title('Generate Video Details')
-form = st.form(key='my-form')
-user_email = form.text_input('Enter registered email :')
-search_term = form.text_input('Enter the search term :')
-submit = form.form_submit_button('Submit')
-st.write("Note:- Enter 'Demo' in registered emailid field To Activate Demo Version")
+    #app_url="https://play.google.com/store/apps/details?id=com.techniquesandtips.berich&hl=en&gl=us"
+    if app_url.find("&") == -1:
+        app_id=app_url[app_url.find("=")+1:]
+    else:
+        app_id=app_url[app_url.find("=")+1:app_url.find("&")]
+
+
+    stop_words = stopwords.words('english')
+
+    result = app(
+        app_id,
+        #'com.parakeetsaspets.budgiebirdcare',
+        lang='en', # defaults to 'en'
+        country='us' # defaults to 'us'
+    )
+
+    st.title("App Details :")
+    st.write("\nTitle :", result['title'])
+    description_sentences=result['description']
+    words=""
+    description_words=""
+    words=word_tokenize(description_sentences)
+    description_words = [word for word in words if word not in (stop_words)]
+
+    fd=FreqDist()
+    for word in description_words:
+        if(word not in ("." , ",","’","!")):
+            fd[word.lower()]+=1
+    st.header("\nTop 5 Most Used Words:")    
+    #fd.most_common(5)
+    for i in range(5):
+        st.write( f"{i+1}. ", fd.most_common(5)[i][0],"-",fd.most_common(5)[i][1],", Density :" ,fd.most_common(5)[i][1]*100/len(words))
+    st.header("\n App Details:")    
+    st.write("\nTitle Length :",len(result['title']))
+    st.write("\nSummary Length :",len(result['summary']))
+    st.write("\nDescription Length :",len(result['description']))
+    st.write("\nInstalls :",result['installs'])
+    st.write("\nVideo in store listing:",result['video'])
+    st.write("\nNumber of Screenshots :",len(result['screenshots']))
+    st.write("\nContains Ads:",result['containsAds'])
+    #st.write("\nDeveloper Email:",result['developerEmail'])
+    st.write("\nNumber of Rating:",result['ratings'])
+    st.write("\nNumber of Reviews:",result['reviews'])
+    st.write("\nAverage Score:",result['score'])
+    st.write("\nNumber of Comments:",len(result['comments']))
+
+    st.header("\n\nLatest Comment Analysis")
+    sid_obj = SentimentIntensityAnalyzer()
+    pos_num=0
+    neu_num=0
+    neg_num=0
+
+    for i in range(len(result['comments'])):
+        sentiment_dict = sid_obj.polarity_scores(result['comments'][i])
+        #st.write("\n\nComment :", result['comments'][i] ) 
+        #st.write("Overall sentiment dictionary is : ", sentiment_dict)
+        #st.write("Sentence Overall Rated As", end = " ")
+     
+        # decide sentiment as positive, negative and neutral
+        if sentiment_dict['compound'] >= 0.05 :
+            #st.write("Positive")
+            pos_num+=1
+     
+        elif sentiment_dict['compound'] <= - 0.05 :
+            #st.write("Negative")
+            neg_num+=1
+     
+        else :
+            #st.write("Neutral")
+            neu_num+=1
+            
+    st.write("\nTotal Comments: ",len(result['comments']))
+    st.write("Total Positive:",pos_num )
+    st.write("Total Negative: ",neg_num)
+    st.write("Total Neutral: ",neu_num)
+
+
+    st.title("\n\nSummary")  
+
+    if(fd.most_common(5)[0][1]*100/len(words) <5.0 or fd.most_common(5)[1][1]*100/len(words) < 5.0 ):
+        st.header("Keyword Density Of Top Words:")
+        st.write("Density of one or both of your top 2 words is less than 5%")
+        st.write("Try to keep a density of more than 5 for top 2 words.")
+        st.write("This will help google understand the main topic of your app and rank accordingly.")
+        st.write("But, donot overdo it by increasing the density beyond 6%") 
+
+    if(len(result['description']) < 3900 ):
+        st.header("Length Of Description")
+        st.write("Available Limit For Description: 4000")
+        st.write("You are using: ", len(result['description']))
+        st.write("So, you are wasting available space.")
+        st.write("A detailed description will help understand google understand and rank your app better.")
+        st.write("\n Note:- In Short Description include a call to action or a strong reason why someone should download your app.")   
+
+    if(result['video'] == None ):
+        st.header("Video In Store Listing")
+        st.write("You must include a video in play store listing.") 
+        st.write("Not just normal video but animated video describing your app and why it should be downloaded.") 
+        st.write("Keep it short and to the point.")
+
+    if neg_num > 0:
+        st.header("Comments/Reviews")
+        if neg_num > pos_num :
+            st.write("You have more negative reviews than positive.\nWork on elliminating negative reviews for better ranking and feedback.")
+        else:
+            st.write("You have more positive comments but still there are few negative comments.")
+        st.write("Negative comments impacts downloads.\nAre you doing something to deal with it?")
+
+    if result['score'] < 4.5:
+        st.header("Rating Score")
+        st.write(f"You have a score of {result['score']}. Work of increasing it to above 4.5 ")
+
+    st.header("\n\nHow we can help you.")     
+    #st.write("1. We can write a better description for your app.")
+    st.write("1. Create 2 special animated video for your app with male/female voice. You can use any of these in play store.")
+    st.write("2. Let you know about a special method to elliminate negative reviews and get more positive reviews.")
+    st.header("[Click Here To Contact Us](https://abhigyandms.com/contact-form/)")
+def send_mail(user_email,sys):
+    receiver = "abhigyandms@gmail.com"
+    body = "App In Use Notification"
+    #filename = "document.pdf"
+    yag = yagmail.SMTP("sonamappsplatform@gmail.com", st.secrets["email_pass"])
+    #yag = yagmail.SMTP("sonamappsplatform@gmail.com")
+    platform = "Youtube"
+    if sys=='G':
+        platform = "Google Play"
+
+    yag.send(
+        to=receiver,
+        subject="New User",
+        contents=f"{user_email} is using {platform} search" 
+        #attachments=filename,
+    )
+
+#st.title('Generate Video Details')
+#form = st.form(key='my-form')
+#user_email = form.text_input('Enter registered email :')
+#search_term = form.text_input('Enter the search term :')
+#submit = form.form_submit_button('Submit')
+#st.write("Note:- Enter 'Demo' in registered emailid field To Activate Demo Version")
+#st.header('Click Submit Button To Start')
+
+
+
+
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header('Generate Youtube Video Details')
+    form = st.form(key='youtube-form')
+    user_email = form.text_input('Enter your email :')
+    search_term = form.text_input('Enter the search term :')
+    submit_youtube = form.form_submit_button('Submit')
+    #st.write("Note:- Enter 'Demo' in registered emailid field To Activate Demo Version")
+    
+
+
+with col2:
+    st.header('Generate Google Play Details')
+    form_gplay = st.form(key='form_gplay')
+    user_email_gplay = form_gplay.text_input('Enter your email :')
+    app_url = form_gplay.text_input('Enter app url :')
+    #search_term = form.text_input('Enter the search term :')
+    submit_gplay = form_gplay.form_submit_button('Submit')
+    #st.write("Note:- Enter 'Demo' in registered emailid field To Activate Demo Version")
+    #st.write('Click Submit Button To Start')
 st.header('Click Submit Button To Start')
+st.write('Note:- Your email is safe and will not be shared with third parties or sold anytime.')
 
-
-
-
-if submit:
-
+if submit_youtube:
     if search_term=="" or user_email=="":
         st.title("Email and Search Term cannot be blank")
     else:
-        validateUser(user_email)
-        
-
-
-
+        #validateUser(user_email) 
+        send_mail(user_email,'Y')
+        mainSearch(search_term,"N")  
     
-    
+if submit_gplay:
+    if app_url=="" or user_email_gplay=="":
+        st.title("Email and App URL cannot be blank")
+    else:
+        send_mail(user_email_gplay,'G')
+        gplayDetails(app_url)
 
     
        
